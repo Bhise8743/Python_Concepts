ce3e17c3fb00584f290b53f6d871eebea6662ca0
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-11-24 20:44:29 +0530

    [Omkar] add : Numbers Patterns

---
+++ Patterns/NumberPattern.py
@@ -0,0 +1,13 @@
+'''
+
+@Author: Omkar Bhise
+
+@Date: 2023-11-24 12:30:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2023-11-24 7:00:00
+
+@Title : to print the Number pattern  star pattern
+
+'''
