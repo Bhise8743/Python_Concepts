6344f05d4da7c33c59e42a3860d40c38f9970b07
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-11-24 20:51:52 +0530

    [Omkar] add : Butterfly patters it takes the input from the user

---
+++ Patterns/ButterFlyPattern.py
@@ -0,0 +1,33 @@
+'''
+
+@Author: Omkar Bhise
+
+@Date: 2023-11-24 12:30:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2023-11-24 7:00:00
+
+@Title : to print the Butterfly star pattern
+
+'''
+
+n = int(input("Enter the number "))
+
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    for j in range((n-i-1)*2):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+
+for i in range(n):
+    for j in range(n-i):
+        print("*",end=" ")
+    for j in range(i*2):
+        print(" ",end=" ")
+    for j in range(n-i):
+        print("*",end=" ")
+    print()
