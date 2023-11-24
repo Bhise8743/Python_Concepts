feaebd954023cf577a08c28df2724f8de3a4a089
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-11-24 13:19:30 +0530

    [Omkar] add : this is a square pattern program taking input form the user

---
+++ Patterns/SquarePattern.py
@@ -0,0 +1,21 @@
+
+'''
+
+@Author: Omkar Bhise
+
+@Date: 2023-11-24 12:30:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2023-11-24 12:30:30
+
+@Title : to print the square star pattern
+
+'''
+
+n = int(input("Enter the size "))
+
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+    print()
