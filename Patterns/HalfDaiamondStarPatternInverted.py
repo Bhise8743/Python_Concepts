74e89a2099781f0b8d7b38ec33bbe5c5d028d4b5
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-11-24 19:22:11 +0530

    [Omkar] add : i am add the HalfDiamondStarPattrnInverted

---
+++ Patterns/HalfDaiamondStarPatternInverted.py
@@ -0,0 +1,29 @@
+'''
+
+@Author: Omkar Bhise
+
+@Date: 2023-11-24 12:30:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2023-11-24 6:30:00
+
+@Title : to print the Half Diamond Inverted star pattern
+
+'''
+
+n = int(input("Enter the number "))
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end=" ")
+    for k in range(i+1):
+        print("*",end=" ")
+    print()
+
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for k in range(n-i-1):
+        print("*",end=" ")
+    print()
