c0dec04962b83b74e64d5535e87c2b7c9240f10e
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-11-24 19:27:31 +0530

    [Omkar] add : Inverted pyramid star pattern it take the input from the user

--- Patterns/InvertedPyramidStarPattern.py
+++ Patterns/InvertedPyramidStarPattern.py
@@ -6,8 +6,17 @@
 
 @Last Modified by: Author Name
 
-@Last Modified time: 2023-11-24 12:30:30
+@Last Modified time: 2023-11-24 6:30:00
 
 @Title : to print the Inverted Pyramid star pattern
 
 '''
+
+n = int(input("Enter the  size "))
+
+for i in range(n):
+    for k in range(i):
+        print(" ",end=" ")
+    for j in range((n-i-1)*2+1):
+        print("*",end=" ")
+    print()
