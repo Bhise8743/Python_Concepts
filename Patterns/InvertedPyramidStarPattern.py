be9dd687152b8647c555421514f6abb80fed2bb0
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-11-24 13:21:41 +0530

    [Omkar] add : this is a Inverted Pyramid pattern program taking input form the user

---
+++ Patterns/InvertedPyramidStarPattern.py
@@ -0,0 +1,13 @@
+'''
+
+@Author: Omkar Bhise
+
+@Date: 2023-11-24 12:30:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2023-11-24 12:30:30
+
+@Title : to print the Inverted Pyramid star pattern
+
+'''
