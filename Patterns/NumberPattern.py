6f4f74d562b7e273d1efcf262710925b8e51f4c0
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-11-24 20:51:10 +0530

    [Omkar] add : number patters it takes the input from the user

--- Patterns/NumberPattern.py
+++ Patterns/NumberPattern.py
@@ -11,3 +11,11 @@
 @Title : to print the Number pattern  star pattern
 
 '''
+
+n = int(input("Enter the number "))
+p=1
+for i in range(n):
+    for j in range(i+1):
+        print(p,end=" ")
+        p = p+1
+    print()
