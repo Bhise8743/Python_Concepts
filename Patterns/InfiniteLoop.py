fe14d2d0e4795ae674a15aaa1eaa7515d834e0eb
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-11-28 12:07:14 +0530

    [Omkar] add : this is infinite loop

---
+++ Patterns/InfiniteLoop.py
@@ -0,0 +1,19 @@
+
+
+'''
+
+@Author: Omkar Bhise
+
+@Date: 2023-11-24 12:30:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2023-11-24 12:30:30
+
+@Title : to print the Infinite Loop
+
+'''
+
+# membersip oprator
+while True:
+    print("Hi")
