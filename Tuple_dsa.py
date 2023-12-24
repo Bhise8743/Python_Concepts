b3d6ba3845b731340151b3ebbeb79e363bb00852
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-12-24 10:22:26 +0530

    [Omkar] Add : Basic of the tuple

--- Tuple_dsa.py
+++ Tuple_dsa.py
@@ -1,5 +1,15 @@
 # nested tuple
-my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+
+l = [12,23,41,52]
+d = {"java" :"programming", "one" : "two"}
+
+my_tuple = ("mouse",l,d, [8, 4, 6], (1, 2, 3))
 print(my_tuple)
+
 my_tuple[1].append(12)
+
+d.update({"three" : "four"})
+
 print(my_tuple)
+
+print(type(my_tuple))
